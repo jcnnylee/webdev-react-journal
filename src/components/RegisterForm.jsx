0c6552b5d8c7778757b2bb513b95d79d416c0b1b
jcnnylee <jennyylee0315@gmail.com>
2025-12-24 12:36:45 -0500

    register form refresh

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,8 +1,11 @@
 import {Formik, Form, Field} from "formik"
-import * as Yup from 'yup'
+import * as Yup from "yup"
+import axios from "axios"
 import {Button, TextField, Box, Stack,} from "@mui/material"
 
 function RegisterForm() {
+
+    // YUP Used to validate form inputs
     const schema = Yup.object({
         email: Yup.string().email("Invalid email").required("Required*"),
         password: Yup.string().matches(/\d/, "Password must contain at least one number").min(6, "Password must be at least 6 characters").max(16, "Password must not exceed 16 characters").required("Required*"),
@@ -10,63 +13,73 @@ function RegisterForm() {
     })
 
     // Register Form UI and validation
-    return <Formik
-        initialValues={{
-            email: "",
-            password:"",
-            confirmPassword:"",
-        }}
-        validationSchema={schema}
-        onSubmit={(values) => {
-            console.log("Form values", values)
-        }}
-        >
-        {({values, errors, touched}) => {
-            console.log("Values", values)
-            console.log("Errors", errors)
-            console.log("Touched", touched)
-        return (
-            <Form>
-                <Box sx={{
-                    display:"flex",
-                    flexDirection: "column",
-                    gap: 2,
-                    width: 300,
-                }}>
-                    <Field as={TextField} 
-                        label="Email" 
-                        type="email" 
-                        name="email" 
-                        error={touched.email && Boolean(errors.email)} // Shows as an error if email has been touched and the email is incorrect or not filled
-                        helperText={touched.email && errors.email}
-                    />
+    return (
+        <Formik
+            initialValues={{
+                email: "",
+                password:"",
+                confirmPassword:"",
+            }}
+            validationSchema={schema}
+            onSubmit={async (values, {resetForm}) => {
+                try {
+                //console.log("Form values", values)
+                    await axios.post("http://localhost:4000/register", values)
+                    resetForm()
 
-                    <Field as={TextField} 
-                        label="Password" 
-                        type="password" 
-                        name="password"
-                        error={touched.password && Boolean(errors.password)}
-                        helperText={touched.password && errors.password}
-                        
-                    />
+                } catch(error) {
+                    console.log(error)
+                }
+            }}
+            >
+            {({values, errors, touched, ...methods}) => {
+                console.log("Values", values)
+                console.log("Errors", errors)
+                console.log("Touched", touched)
+                console.log(methods)
+                return (
+                    <Form>
+                        <Box sx={{
+                            display:"flex",
+                            flexDirection: "column",
+                            gap: 2,
+                            width: 300,
+                        }}>
+                            <Field as={TextField} 
+                                label="Email" 
+                                type="email" 
+                                name="email" 
+                                error={touched.email && Boolean(errors.email)} // Shows as an error if email has been touched and the email is incorrect or not filled
+                                helperText={touched.email && errors.email}
+                            />
 
-                    <Field as={TextField} 
-                        label="Confirm Password" 
-                        type="password" 
-                        name="confirmPassword"
-                        error={touched.confirmPassword && Boolean(errors.confirmPassword)}
-                        helperText={touched.confirmPassword && errors.confirmPassword}
-                    />
+                            <Field as={TextField} 
+                                label="Password" 
+                                type="password" 
+                                name="password"
+                                error={touched.password && Boolean(errors.password)}
+                                helperText={touched.password && errors.password}
+                                
+                            />
 
-                    <Button 
-                        type="submit" 
-                        variant="contained"
-                    >Register
-                    </Button>
-                </Box>
-            </Form>
-        )
-        }}
-    </Formik>
+                            <Field as={TextField} 
+                                label="Confirm Password" 
+                                type="password" 
+                                name="confirmPassword"
+                                error={touched.confirmPassword && Boolean(errors.confirmPassword)}
+                                helperText={touched.confirmPassword && errors.confirmPassword}
+                            />
+
+                            <Button 
+                                type="submit" 
+                                variant="contained"
+                            >Register
+                            </Button>
+                    </Box>
+                </Form>
+            )
+            }}
+        </Formik>
+    )
 }
 export default RegisterForm
