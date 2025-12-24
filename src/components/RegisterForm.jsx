f2fcc45a18704f70b39efe4161ce3c529cee5b20
jcnnylee <jennyylee0315@gmail.com>
2025-12-24 11:53:22 -0500

    Yup added

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,20 +1,30 @@
 import {Formik, Form, Field} from "formik"
-
+import * as Yup from 'yup'
 import {Button, TextField, Box, Stack,} from "@mui/material"
 
 function RegisterForm() {
-  return <Formik
+    const schema = Yup.object({
+        email: Yup.string().email("Invalid email").required("Required*"),
+        password: Yup.string().matches(/\d/, "Password must contain at least one number").min(6, "Password must be at least 6 characters").max(16, "Password must not exceed 16 characters").required("Required*"),
+        confirmPassword: Yup.string().oneOf([Yup.ref("password")], "Passwords must match").required("Required*")
+    })
+
+    // Register Form UI and validation
+    return <Formik
         initialValues={{
             email: "",
             password:"",
             confirmPassword:"",
         }}
+        validationSchema={schema}
         onSubmit={(values) => {
             console.log("Form values", values)
         }}
         >
-        {() => {
-
+        {({values, errors, touched}) => {
+            console.log("Values", values)
+            console.log("Errors", errors)
+            console.log("Touched", touched)
         return (
             <Form>
                 <Box sx={{
@@ -26,19 +36,26 @@ function RegisterForm() {
                     <Field as={TextField} 
                         label="Email" 
                         type="email" 
-                        name="email"
+                        name="email" 
+                        error={touched.email && Boolean(errors.email)} // Shows as an error if email has been touched and the email is incorrect or not filled
+                        helperText={touched.email && errors.email}
                     />
 
                     <Field as={TextField} 
                         label="Password" 
                         type="password" 
                         name="password"
+                        error={touched.password && Boolean(errors.password)}
+                        helperText={touched.password && errors.password}
+                        
                     />
 
                     <Field as={TextField} 
                         label="Confirm Password" 
                         type="password" 
                         name="confirmPassword"
+                        error={touched.confirmPassword && Boolean(errors.confirmPassword)}
+                        helperText={touched.confirmPassword && errors.confirmPassword}
                     />
 
                     <Button 
